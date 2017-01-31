e7db4b8ab4710f44bb0122ba909c28374691a980
杨弘宇 <unixzii@gmail.com>
2017-01-31 11:03:38 +0800

    Initial Commit

---
+++ Snapin/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  Snapin
+//
+//  Created by 杨弘宇 on 2017/1/31.
+//  Copyright © 2017年 杨弘宇. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
